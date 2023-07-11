586793571014b78aef5a204988067476323839c5
Mushaim <mushaimk01@gmail.com>
2023-07-11 13:13:44 +0000

    addUser in session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,19 +1,63 @@
-import NextAuth,{ type NextAuthOptions } from "next-auth";
- import  CredentialsProvider  from "next-auth/providers/credentials"; 
- import { PrismaClient } from "@prisma/client";
- 
- const prisma = new PrismaClient();
-  export const authOptions: NextAuthOptions = { 
-    session: { strategy: 'jwt' }, 
-    providers: [ CredentialsProvider(
-        { name: 'Sign in',
-         credentials: { email: { label: 'Email', type: 'email', placeholder: 'hello@example.com' },
-         password: {label: 'Password',type: 'password'} }, 
-         async authorize(credentials){
-             if(!credentials?.email || !credentials.password){ return null } 
-             const user = await prisma.user.findUnique({ where: { email:credentials.email } })
-             if(!user){ return null } 
-             if(user?.password!==credentials?.password){ return null } 
-             return{ id: user.id+ '', email:user.email, name: user.name } } }) ] } 
-             const handler = NextAuth(authOptions)
-             export { handler as Get,handler as POST}
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import { PrismaClient, User } from '@prisma/client';
+import { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth/next';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { z } from 'zod';
+import bcrypt from 'bcrypt';
+
+const loginUserSchema = z.object({
+ email: z.string().regex(/^[a-z0-9_-]{3,15}$/g, 'Invalid email'),
+  password: z.string().min(5, 'Password should be minimum 5 characters'),
+});
+const prisma = new PrismaClient();
+
+const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    CredentialsProvider({
+      credentials: {
+        email: { type: 'email', placeholder: 'test@test.com' },
+        password: { type: 'password', placeholder: 'Pa$$w0rd' },
+      },
+      async authorize(credentials, req) {
+        const { email, password } = loginUserSchema.parse(credentials);
+        const user = await prisma.user.findUnique({
+          where: { email:email },
+        });
+        if (!user) return null;
+
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+
+        if (!isPasswordValid) return null;
+
+        return user;
+      },
+    }),
+  ],
+  callbacks: {
+    session({ session, token }) {
+      session.user.id = token.id;
+      session.user.email = token.email;
+      return session;
+    },
+    jwt({ token, account, user }) {
+      if (account) {
+        token.accessToken = account.access_token;
+        token.id = user.id;
+        token.email = (user as User).email;
+        console.log({ user });
+      }
+      return token;
+    },
+  },
+  pages: {
+    signIn: '/LoginContainer',
+  },
+  session: {
+    strategy: 'jwt',
+  },
+  secret: process.env.JWT_SECRET,
+};
+
+export default NextAuth(authOptions);
